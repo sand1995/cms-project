fd3a868aebdd819fbecdb4d04e1d19b6199800dc
Andres Santiago Arias P <zant20@gmail.com>
2019-05-06 19:44:03 -0500

    avance

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const app = express();
 const path = require('path');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
-
+//mongo conection
 mongoose.connect('mongodb://localhost:27017/cms', {useNewUrlParser: true}).then((db)=>{
     console.log('MongoDB Connected');
     
@@ -14,14 +15,19 @@ mongoose.connect('mongodb://localhost:27017/cms', {useNewUrlParser: true}).then(
 
 
 app.use(express.static(path.join(__dirname,'public')));
-
+//set view engine
 app.engine('handlebars', exphbs({defaultLayout: 'home'}));
 app.set('view engine', 'handlebars');
 
+//body parser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json);
+
+//load routes
 const routesHome = require('./routes/home/index');
 const routesAdmin = require('./routes/admin/index');
 const routesPosts = require('./routes/admin/posts');
-
+//use routes
 app.use('/',routesHome);
 app.use('/admin',routesAdmin);
 app.use('/admin/posts',routesPosts);
--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../../models/Post')
 
 router.all('/*',(req,res,next)=>{
     req.app.locals.layout = 'admin';
@@ -8,11 +9,34 @@ router.all('/*',(req,res,next)=>{
 
 
 router.get('/',(req,res)=>{
-    res.render('admin/posts');
+    res.render('admin/posts/index');
 });
 
 router.get('/create',(req,res)=>{
     res.render('admin/posts/create');
+});
+
+router.post('/create',(req,res)=>{
+    let allowComments = true;
+
+    if(req.body.allowComments){
+        allowComments= true;
+    }else{
+        allowComments= false;
+    }
+    const newPost = new Post({
+        title:req.body.title,
+        status: req.body.status,
+        allowComments: allowComments,
+        body : req.body.body
+    })
+    newPost.save().then(savedPost=>{
+        console.log(savedPost);
+        
+        res.redirect('/admin/posts');
+    }).catch(err=>{
+        console.log('post no saved'); 
+    });
 });
 
 
