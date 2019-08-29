01c8ecee707c027815e7067dcc0890bc30109ba4
Andres Santiago Arias P <zant20@gmail.com>
2019-08-28 19:44:11 -0500

    User Registration ok

--- models/User.js
+++ models/User.js
@@ -2,11 +2,23 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const CategorySchema = new Schema({
-    category:{
+const UserSchema = new Schema({
+    firstName:{
+        type: String,
+        require: true
+    },
+    lastName:{
+        type: String,
+        require: true
+    },
+    email:{
+        type: String,
+        require: true
+    },
+    password:{
         type: String,
         require: true
     },
 });
 
-module.exports=mongoose.model('categories', CategorySchema);
+module.exports=mongoose.model('users', UserSchema);
--- routes/home/index.js
+++ routes/home/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../../models/Post');
 const Category = require('../../models/Category');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 router.all('/*',(req,res,next)=>{
     req.app.locals.layout = 'home';
@@ -26,6 +28,43 @@ router.get('/login',(req,res)=>{
 
 router.get('/register',(req,res)=>{
     res.render('home/register');
+});
+
+router.post('/register',(req,res)=>{
+    let errors = [];
+    if(req.body.password != req.body.passwordConfirm){
+        errors.push({message: "Password fields don't match"});
+        res.render('home/register',{
+            errors : errors,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email
+        });
+            
+    }else{
+        User.findOne({email: req.body.email}).then(userExist=>{
+            if(!userExist){
+                const newUser = new User({
+                    fistName: req.body.fistName,
+                    lastName: req.body.lastName,
+                    email: req.body.email,
+                    password: req.body.password,
+                });
+                bcrypt.genSalt(10, (err, salt)=>{
+                    bcrypt.hash(newUser.password, salt, (err, hash)=>{
+                        newUser.password=hash;
+                            newUser.save().then(userSaved=>{
+                                req.flash('success_message', 'You are now registered, please login');
+                                res.redirect('/login');
+                        });
+                    });
+                });
+            }else{
+                req.flash('error_message','The email exist');
+                res.redirect('/login');
+            }
+        });
+    }
 });
 
 router.get('/post/:id',(req,res)=>{
